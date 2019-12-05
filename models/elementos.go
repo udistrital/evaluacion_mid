26786dbd8ccb48f641f3615ff4b2fac1561320fb
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-12-05 18:44:47 -0500

    se verifica si existe una clasificacion antes de ingresarla #9

--- models/elementos.go
+++ models/elementos.go
@@ -52,3 +52,32 @@ func GetElementoMaptoStringToArray(objeto interface{}, item string) (ElementosAr
 	}
 
 }
+
+// GetElementoMaptoStringToMapArray ...
+func GetElementoMaptoStringToMapArray(objeto interface{}) (ElementosMapArray []map[string]interface{}, outputError interface{}) {
+	value := reflect.ValueOf(objeto)
+	// ArrayRespuesta := make([]string, 0)
+	// var resuesta string
+	// var elementoIndividual map[string]interface{}
+	ArrayElementos := make([]map[string]interface{}, 0)
+	if value.IsValid() {
+		if value.Len() > 0 {
+			for i := 0; i < value.Len(); i++ {
+				aux := value.Index(i).Interface().(map[string]interface{})
+				ArrayElementos = append(ArrayElementos, aux)
+				// resuesta = fmt.Sprintf("%v", aux[item])
+				// ArrayRespuesta = append(ArrayRespuesta, resuesta)
+			}
+		}
+		if value.Len() == 0 {
+			errorElementos := CrearError("Longitud cero de elmentos")
+			return nil, errorElementos
+		}
+		return ArrayElementos, nil
+
+	} else {
+		errorElementos := CrearError("json con datos incompletos")
+		return nil, errorElementos
+	}
+
+}
--- models/plantilla.go
+++ models/plantilla.go
@@ -2,9 +2,11 @@ package models
 
 import (
 	"fmt"
+	"reflect"
+
+	"github.com/astaxie/beego/logs"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/logs"
 	"github.com/udistrital/utils_oas/request"
 )
 
@@ -19,11 +21,15 @@ func IngresoPlantilla(plantilla map[string]interface{}) (plantillaResult map[str
 		"Usuario":     plantillaArray[0]["Usuario"],
 	}
 	plantillaBase, errPlantilla := PostPlantilla(plantillaIngresada)
-	fmt.Println(plantillaBase)
+	fmt.Println(plantillaBase["Id"])
 	if errPlantilla != nil {
 		return nil, errPlantilla
 	} else {
+		clasificacionesResult, errClasificaciones := PostClasificacion(plantillaArray[0]["Clasificacion"])
+		fmt.Println(clasificacionesResult)
+		fmt.Println(errClasificaciones)
 		return plantillaBase, nil
+
 	}
 }
 
@@ -31,11 +37,72 @@ func IngresoPlantilla(plantilla map[string]interface{}) (plantillaResult map[str
 func PostPlantilla(plantilla map[string]interface{}) (plantillaResult map[string]interface{}, outputError interface{}) {
 	var plantillaPost map[string]interface{}
 	error := request.SendJson(beego.AppConfig.String("evaluacion_crud_url")+"plantilla", "POST", &plantillaPost, plantilla)
-	logs.Error(error)
-	logs.Info(plantillaPost)
 	if error != nil {
 		return nil, error
 	} else {
 		return plantillaPost, nil
 	}
+}
+
+// PostClasificacion ...
+func PostClasificacion(clasificaciones interface{}) (clasificacionesResult map[string]interface{}, outputError interface{}) {
+	fmt.Println(clasificaciones)
+	// fmt.Println(clasificaciones.(map[string]interface{}))
+	clasificacionesMap, errMap := GetElementoMaptoStringToMapArray(clasificaciones)
+	if clasificacionesMap != nil {
+		// fmt.Println(clasificacionesMap)
+		// fmt.Println(clasificacionesMap[0]["Nombre"])
+		for i := 0; i < len(clasificacionesMap); i++ {
+			getClasificacion := GetClasificacionParametrica(clasificacionesMap[i])
+			if getClasificacion != nil {
+				logs.Info("si existe clasificacion para", clasificacionesMap[i]["Nombre"])
+			} else {
+				logs.Info("NO existe clasificacion para", clasificacionesMap[i]["Nombre"])
+
+			}
+		}
+	} else {
+		fmt.Println("valio verga", errMap)
+	}
+	// for _, clasificacion := range clasificaciones.(map[string]interface{})) {
+	// 	fmt.Println(clasificacion)
+	// }
+	return nil, nil
+}
+
+// PostClasificacionParametrica ...
+func PostClasificacionParametrica() {
+
+}
+
+// GetClasificacionParametrica ...
+func GetClasificacionParametrica(clasificacion map[string]interface{}) (clasificacionesResult []map[string]interface{}) {
+	var clasificacionGet []map[string]interface{}
+	// fmt.Println(clasificacion["Nombre"])
+	// var infoClasificacion []map[string]interface{}
+	query := "Nombre:" + fmt.Sprintf("%v", clasificacion["Nombre"]) + ",LimiteInferior:" + fmt.Sprintf("%v", clasificacion["limite_inferior"]) + ",LimiteSuperior:" + fmt.Sprintf("%v", clasificacion["limite_superior"])
+	fmt.Println("query", query)
+	error := request.GetJson(beego.AppConfig.String("evaluacion_crud_url")+"clasificacion?query="+query, &clasificacionGet)
+	if error != nil {
+		logs.Error(error)
+		return nil
+	} else {
+		aux := reflect.ValueOf(clasificacionGet[0])
+		fmt.Println("aux: ", aux.Len())
+		if aux.IsValid() {
+			if aux.Len() > 0 {
+				return clasificacionGet
+			} else {
+				return nil
+			}
+		} else {
+			return nil
+		}
+	}
+
+}
+
+// PostClasificacionPlantilla ...
+func PostClasificacionPlantilla() {
+
 }
